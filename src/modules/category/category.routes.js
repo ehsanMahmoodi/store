c250c6ec030afd6e389543c1507c9672cdb6eea1
Ehsan MahmoodAbadi <mahmoodie24m@gmail.com>
2024-05-31 10:36:22 +0330

    add Authorization to create-update-remove category routes

--- src/modules/category/category.routes.js
+++ src/modules/category/category.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const { CategoryController } = require("./category.controller");
+const { Authorization } = require("../../common/guard/authorization.guard");
 
 const router = Router();
-router.post("/new", CategoryController.create);
+router.post("/new", Authorization, CategoryController.create);
 router.get("/get", CategoryController.getAll);
 router.get("/find/:id", CategoryController.find);
-router.patch("/update/:id", CategoryController.update);
-router.delete("/remove/:id", CategoryController.remove);
+router.patch("/update/:id", Authorization, CategoryController.update);
+router.delete("/remove/:id", Authorization, CategoryController.remove);
 module.exports = {
   CategoryRouter: router,
 };
