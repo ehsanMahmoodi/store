577c53ba3b320556cc4154be8e5e9780e2b50476
Ehsan MahmoodAbadi <mahmoodie24m@gmail.com>
2024-06-03 22:09:46 +0330

    Checking if the option name is duplicated in each product

--- src/modules/option/option.messages.js
+++ src/modules/option/option.messages.js
@@ -1,7 +1,7 @@
 const OptionMessages = Object.freeze({
   Created: "آپشن با موفقیت ایجاد شد.",
   CreatedError: "مشکلی در ایجاد آپشن به وجود آمده.",
-  Conflict: "عنوان یا key آپشن تکراری می باشد.",
+  Conflict: "عنوان یا key آپشن در محصول مورد نظر تکراری می باشد.",
   NotFound: "آپشن یافت نشد.",
   Updated: "آپشن با موفقیت بروزرسانی شد.",
   UpdatedError: "مشکلی در بروزرسانی آپشن به وجود آمده.",
